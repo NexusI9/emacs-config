4fb3876a1adff723649f5770622461c829bb137b
NexusI9 <nassim.elkhantour@gmail.com>
2025-06-25 13:18:46 +0800

    update git ignore and rm cache

--- dashboard.el
+++
@@ -1,9 +0,0 @@
-;; Display dashboard on start
-(use-package dashboard
-  :ensure t
-  :init
-  (dashboard-setup-startup-hook))
-
-(setq dashboard-startup-banner "~/.emacs.d/media/logo.svg")
-
-(setq dashboard-items '((recents   . 10)))
--- elpa/esmond-theme-20230526.124/esmond-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "esmond-theme" "20230526.124"
-  "Esmond dark theme."
-  ()
-  :url "https://github.com/xcatalyst/esmond-theme"
-  :commit "22b414599fbda46f90a210e846ca64b8427dd3f2"
-  :revdesc "22b414599fbd"
-  :authors '(("ağan Korkmaz" . "cagankorkmaz35@gmail.com"))
-  :maintainers '(("ağan Korkmaz" . "cagankorkmaz35@gmail.com")))
