a165799696089531038b9d462664d66a2a65e2cc
NexusI9 <nassim.elkhantour@gmail.com>
2026-04-09 15:46:51 +0800

    add smart formatter

--- code/format.el
+++ code/format.el
@@ -1,5 +1,30 @@
+
+;; Beautify
+;; https://github.com/lassik/emacs-format-all-the-code/blob/master/format-all.el
+;; (use-package format-all
+;;   :ensure t
+;;   :preface
+;;   (defun nk/format-code ()
+;;     "Auto-format whole buffer."
+;;     (interactive)
+;;     (if (derived-mode-p 'prolog-mode)
+;;         (prolog-indent-buffer)
+;;       (format-all-buffer)))
+;;   :config
+;;   (global-set-key (kbd "M-F") #'nk/format-code)
+;;   (add-hook 'prog-mode-hook #'format-all-ensure-formatter))
+
+
+(use-package format-all
+  :commands format-all-mode
+  :hook (prog-mode . format-all-mode)
+  :config
+  (setq-default format-all-formatters
+                '(("CSS"     (prettier)))))
+
+
 (defun my/format-buffer ()
-  "Format with LSP when supported, otherwise use format-all interactively."
+  "Format current buffer with LSP if available, else use format-all."
   (interactive)
   (cond
    ((and (bound-and-true-p lsp-mode)
@@ -11,14 +36,5 @@
     (call-interactively #'format-all-buffer))
    (t
     (user-error "No supported formatter found for this buffer"))))
-
-(with-eval-after-load 'format-all
-  (add-hook 'format-all-mode-hook #'format-all-ensure-formatter))
-
+ 
 (global-set-key (kbd "M-F") #'my/format-buffer)
-
-;; Define prettier as default formatter for css mode since language server doesn't support formatting
-(with-eval-after-load 'format-all
-  (setf (alist-get 'css-mode format-all-default-formatters) '(prettier))
-  (setf (alist-get 'scss-mode format-all-default-formatters) '(prettier))
-  (setf (alist-get 'less-css-mode format-all-default-formatters) '(prettier)))
